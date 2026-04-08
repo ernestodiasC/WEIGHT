234eb6394f84fb82a74a2893477a102c8f6a45c7
Ernesto Dias Cecilio <arrozfelino.ios@gmail.com>
2026-04-08 19:50:22 -0300

    Chance do item aparecer

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ let ItemPool={
         {name:"Three",weight:.5},
         {name:"Four",weight:.25},
         {name:"Five",weight:.3},
+        {name:"Six",weight:.4},
     ],
 }
 ItemPool.max_weight=0
@@ -12,7 +13,8 @@ for(let i=0;i<ItemPool.list.length;i++){
     ItemPool.max_weight+=ItemPool.list[i].weight
 }
 
-const textid=document.getElementById("result")
+const resultid=document.getElementById("result")
+const oddid=document.getElementById("odd")
 
 getItem=function(){
     let random=Math.random()*ItemPool.max_weight
@@ -20,7 +22,9 @@ getItem=function(){
     while(true){
         random-=ItemPool.list[i].weight
         if(random<0){
-            textid.textContent=ItemPool.list[i].name
+            oddid.textContent=ItemPool.list[i].weight/ItemPool.max_weight*100
+            oddid.textContent+="%"
+            resultid.textContent=ItemPool.list[i].name
             console.log(ItemPool.list[i])
             return ItemPool.list[i]
         }
